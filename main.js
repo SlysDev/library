2ac96e862599718f42ca4f875d39c84183cbf9c4
Jeremy Kamber <jeremynkamber@gmail.com>
2022-01-09 20:33:20 -0500

    Finished UI, added delete button, changed some of the functions, got the book list ui to work

--- main.js
+++ main.js
@@ -2,11 +2,31 @@ let display = document.querySelector(".book-display");
 
 let bookContainer = document.createElement("div");
 
-let formContainer = document.querySelector(".form-container");
+let bookInfoForm = document.querySelector("#book-info-form");
 
 let addBtn = document.querySelector("#add-btn");
 
 let container = document.querySelector("main");
+
+let submitBtn = document.createElement("button");
+
+let data = [];
+
+let titleInput = document.createElement("input");
+
+let descriptionInput = document.createElement("input");
+
+let authorInput = document.createElement("input");
+
+let pagesInput = document.createElement("input");
+
+let titleText = document.createElement("h3");
+
+let authorText = document.createElement("h3");
+
+let pagesText = document.createElement("h3");
+
+let descriptionText = document.createElement("h3");
 
 bookContainer.classList.add("book_container");
 
@@ -14,87 +34,114 @@ bookContainer.innerHTML = "";
 
 let myLibrary = [];
 
+let body = document.querySelector("body");
+
+// Display a hlep message if there are no books in the library
+
 function Book(title, author, desc, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.desc = desc;
-  this.pages = pages;
-  this.read = read;
+    this.title = title;
+    this.author = author;
+    this.desc = desc;
+    this.pages = pages;
+    this.read = read;
+}
+
+function error() {
+    body.classList.add("error");
 }
 
 function createForm() {
-    formContainer.style.display = 'flex';
-  let bookForm = document.createElement("form");
-    let bookFormTitles = document.createElement('div');
-    let bookFormInputs = document.createElement('div');
-    bookForm.classList.add('book-form')
-    bookFormTitles.classList.add('book-form-section');
-    bookFormInputs.classList.add('book-form-section');
-    formContainer.appendChild(bookForm);
-
-  let titleInput= document.createElement("input");
-  titleInput.setAttribute("type", "text");
-  titleInput.setAttribute("name", "title");
-  bookFormInputs.appendChild(titleInput);
-  let descriptionInput= document.createElement("input");
-  descriptionInput.setAttribute("type", "text");
-  descriptionInput.setAttribute("name", "description");
-  bookFormInputs.appendChild(descriptionInput);
-  let authorInput = document.createElement("input");
-  authorInput.setAttribute("type", "text");
-  authorInput.setAttribute("name", "author");
-  bookFormInputs.appendChild(authorInput)
-  let pagesInput = document.createElement("input");
-  pagesInput.setAttribute("type", "text");
-  pagesInput.setAttribute("name", "pages");
-  bookFormInputs.appendChild(pagesInput);
-    let titleText = document.createElement('h2');
+    container.style.display = "none";
+    bookInfoForm.style.display = "block";
+    let bookInfoFormTitles = document.createElement("div");
+    let bookInfoFormInputs = document.createElement("div");
+    let formTextSection = document.createElement("div");
+    formTextSection.classList.add("form-section");
+    let formSubmitSection = document.createElement("div");
+    formSubmitSection.classList.add("form-section");
+    bookInfoFormTitles.classList.add("form-titles");
+    bookInfoFormInputs.classList.add("form-inputs");
+    bookInfoForm.appendChild(formTextSection);
+    bookInfoForm.appendChild(formSubmitSection);
+    titleInput.setAttribute("type", "text");
+    titleInput.setAttribute("name", "title");
+    bookInfoFormInputs.appendChild(titleInput);
+    descriptionInput.setAttribute("type", "text");
+    descriptionInput.setAttribute("name", "description");
+    bookInfoFormInputs.appendChild(descriptionInput);
+    authorInput.setAttribute("type", "text");
+    authorInput.setAttribute("name", "author");
+    bookInfoFormInputs.appendChild(authorInput);
+    pagesInput.setAttribute("type", "text");
+    pagesInput.setAttribute("name", "pages");
+    bookInfoFormInputs.appendChild(pagesInput);
     titleText.textContent = "Title";
-    let descriptionText = document.createElement('h2');
     descriptionText.textContent = "Description";
-    let authorText = document.createElement('h2');
     authorText.textContent = "Author";
-    let pagesText = document.createElement('h2');
     pagesText.textContent = "Pages";
-    bookFormTitles.appendChild(titleText);
-    bookFormTitles.appendChild(descriptionText);
-    bookFormTitles.appendChild(authorText);
-    bookFormTitles.appendChild(pagesText);
-
-    bookForm.appendChild(bookFormTitles);
-    bookForm.appendChild(bookFormInputs);
-    let submitBtn = document.createElement('button');
+    bookInfoFormTitles.appendChild(titleText);
+    bookInfoFormTitles.appendChild(descriptionText);
+    bookInfoFormTitles.appendChild(authorText);
+    bookInfoFormTitles.appendChild(pagesText);
+    formTextSection.appendChild(bookInfoFormTitles);
+    formTextSection.appendChild(bookInfoFormInputs);
+    submitBtn.setAttribute("type", "submit");
     submitBtn.textContent = "Submit";
-    formContainer.appendChild(submitBtn);
-}
-
-function logBook() {
-  createForm();
-  container.style.display = "none";
-}
-
-function processBook() {
-    container.style.display = "block";
-}
-
-function addBookToLibrary(book) {
-  myLibrary.push(book);
-  displayBooks();
+    formSubmitSection.appendChild(submitBtn);
 }
 
 function displayBooks() {
-  myLibrary.forEach((book) => {
-    let bookContainer = document.createElement("div");
-    bookContainer.classList.add("book_container");
-    let title = document.createElement("h2");
-    title.textContent = book.title;
-    let description = document.createElement("p");
-    description.textContent = book.desc;
-  });
+    display.textContent = "";
+    myLibrary.forEach(function (book) {
+        let bookContainer = document.createElement("div");
+        bookContainer.classList.add("book-container");
+        let title = document.createElement("h2");
+        title.textContent = book.title;
+        let description = document.createElement("p");
+        description.textContent = book.desc;
+        let deleteBtn = document.createElement("button");
+        deleteBtn.id = "small-button";
+        deleteBtn.textContent = "x";
+        deleteBtn.addEventListener("click", function () {
+            display.removeChild(bookContainer);
+            myLibrary.splice(book.indexOf(book));
+        });
+        let readBtn = document.createElement("button");
+        readBtn.class = "small-button";
+        bookContainer.appendChild(title);
+        bookContainer.appendChild(description);
+        bookContainer.appendChild(deleteBtn);
+        display.appendChild(bookContainer);
+    });
 }
 
 addBtn.addEventListener("click", function () {
-  logBook();
+    createForm();
 });
 
-
+submitBtn.addEventListener("click", function () {
+    if (
+        titleInput.value == "" ||
+        descriptionInput.value == "" ||
+        authorInput.value == "" ||
+        pagesInput.value == ""
+    ) {
+        return;
+    }
+    let book = new Book(
+        titleInput.value,
+        descriptionInput.value,
+        authorInput.value,
+        pagesInput.value,
+        false
+    );
+    console.log(book);
+    myLibrary.push(book);
+    displayBooks();
+    titleInput.value = "";
+    descriptionInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    bookInfoForm.style.display = "none";
+    container.style.display = "flex";
+});
